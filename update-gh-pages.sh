2725cad573e69ef2a2633b6a24c12f8e8d48b8ef
Steven <ys2833@columbia.edu>
2018-07-21 19:56:24 -0400

    update gh-pages script

---
+++ update-gh-pages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+set -o xtrace
+git checkout master
+git checkout -B gh-pages
+make
+rm -rf .gitignore .gitmodules
+git add Pxtone.js pxtnDecoder.js emDecoder.wasm .gitmodules .gitignore
+git commit -m "update gh-pages"
+git push -f
+git checkout master
