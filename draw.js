972dd955080172a1c7a41578d5327bce70ee9a3d
Steven <ys2833@columbia.edu>
2018-07-21 18:54:14 -0400

    simple playback

--- draw.js
+++ draw.js
@@ -11,53 +11,75 @@ let Player = function () {
   this.measureWidth = 192;
 }
 
+function drawUnitNote(ctx, x, y, w) {
+  ctx.fillRect(x, y-1, w, 2);
+  ctx.fillRect(x, y-2, Math.max(w-1, 0), 4);
+  ctx.fillRect(x, y-3, Math.max(Math.min(2, w), 0), 6);
+  ctx.fillRect(x, y-5, 1, 10);
+}
+
+function middleSnap(x) { return Math.floor(x) + 0.5; }
+
 Player.prototype.draw = function () {
   // - back -
+  ctx.save();
+  // global transform
   ctx.fillStyle = "#000010";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let shiftX = canvas.width/2;
+  ctx.translate(shiftX, 0);
+  ctx.scale(2, 2);
 
   // time offset
-  let beatOffset = (this.audioCtx.currentTime - this.startTime) *
-    this.master.beatTempo / 60;
-  // beatOffset = Math.floor(beatOffset);
-  let xOffset = beatOffset / this.master.beatNum * this.measureWidth;
-  ctx.save();
-  ctx.translate(-xOffset, 0);
+  let currentTime = this.audioCtx.currentTime - this.startTime;
+  let currentBeat = currentTime * this.master.beatTempo / 60;
+  let currentClock = currentBeat * this.master.beatClock;
+  let playX = middleSnap(currentBeat / this.master.beatNum) * this.measureWidth;
+
+  ctx.translate(-playX, 0);
+
+  let canvasOffsetX = playX - shiftX;
 
   // - rulers -
+  let start; // so that only the rulers in the scroll view are drawn
   // beat
   ctx.fillStyle = "#808080";
-  for (let i = 0; i < canvas.width; i += this.measureWidth / this.master.beatNum)
-    ctx.fillRect(i, 0, 1, canvas.height);
+  let beatWidth = this.measureWidth / this.master.beatNum;
+  start = Math.floor(canvasOffsetX / beatWidth);
+  for (let i = 0; i < canvas.width / beatWidth + 1; ++i)
+    ctx.fillRect((i + start) * beatWidth, 0, 1, canvas.height);
 
   // measure
   ctx.fillStyle = "#F0F0F0";
-  for (let i = 0; i < canvas.width; i += this.measureWidth)
-    ctx.fillRect(i, 0, 1, canvas.height);
+  start = Math.floor(canvasOffsetX / this.measureWidth);
+  for (let i = 0; i < canvas.width / this.measureWidth + 1; ++i)
+    ctx.fillRect((i + start) * this.measureWidth, 0, 1, canvas.height);
 
   // - unit rows -
+  start = canvasOffsetX;
   ctx.fillStyle = "#400070";
   for (let i = 0; i < this.evels.unitNum; ++i) {
-    ctx.fillRect(0, i*16 + 1, canvas.width, 15);
+    ctx.fillRect(start, i*16 + 1, canvas.width, 15);
   }
 
   // notes
   ctx.fillStyle = "#F08000";
+  // TODO: use interval tree to get the relevant notes to render
+  let clockPerPx = this.master.beatClock * this.master.beatNum / this.measureWidth;
   for (let e of this.evels.evels) {
     if (e.kind != "ON")
       continue;
-    ctx.save();
-    let clockPerPx = this.master.beatClock * this.master.beatNum / this.measureWidth;
-    ctx.translate(e.clock / clockPerPx, e.unit_no*16 + 8);
     let w = e.value / clockPerPx;
-    ctx.fillRect(0, -1, w, 2);
-    ctx.fillRect(0, -2, Math.max(w-1, 0), 4);
-    ctx.fillRect(0, -3, Math.max(Math.min(2, w), 0), 6);
-    ctx.fillRect(0, -5, 1, 10);
-    ctx.restore();
+    if (e.clock <= currentClock && e.clock + e.value > currentClock) {
+      ctx.save();
+      ctx.fillStyle = "#FFF000";
+    }
+    drawUnitNote(ctx, e.clock / clockPerPx, e.unit_no * 16 + 8, w);
+    if (e.clock <= currentClock && e.clock + e.value > currentClock) ctx.restore();
     // unit_no, value, clock
   }
 
+  ctx.restore();
   ctx.restore();
 }
 
