4e0780d30d7efaac549a08d82d8eaeed6c1e5afc
Steven <ys2833@columbia.edu>
2018-07-22 11:41:51 -0400

    local server

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 #test on python 3.4 ,python of lower version  has different module organization.
+import argparse
 import http.server
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 
-PORT = 8080
+parser = argparse.ArgumentParser(description='Serve player webpage')
+parser.add_argument('--host', help='address to serve on', type=str, default='127.0.0.1', action='store')
+parser.add_argument('--port', help='port to serve on',    type=int, default=8080,        action='store')
+args = parser.parse_args()
 
 Handler = http.server.SimpleHTTPRequestHandler
 
@@ -21,7 +25,7 @@ Handler.extensions_map={
     }
 
 socketserver.TCPServer.allow_reuse_address = True
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = socketserver.TCPServer((args.host, args.port), Handler)
 
-print("serving at port", PORT)
+print("serving at %s:%d" % (args.host, args.port))
 httpd.serve_forever()
